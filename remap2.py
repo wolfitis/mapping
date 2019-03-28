531961105422b7dfaaf63b3dadc65f9e2280c761
wolfitis <webblog123@gmail.com>
2019-03-28 00:54:54 -0700

    v1.1.2.1 topology input from file

--- remap2.py
+++ remap2.py
@@ -13,13 +13,18 @@ import ast
 
 qbits = [[0,1,2,3,4], [0,0,0,0,0]]
 
+# reading of topology
+fo = open('topology.txt')
+str2 = fo.readline()
+g = dict(ast.literal_eval(str2))
+
 # mapping of hardware topology
-g = {0:[1],
-1:[0,2],
-2:[1,3],
-3:[2,4],
-4:[3]
-}
+# g = {0:[1],
+# 1:[0,2],
+# 2:[1,3],
+# 3:[2,4],
+# 4:[3]
+# }
 
 # hardware topology 
 topology = g
